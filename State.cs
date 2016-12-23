c6eaba6efe0334aa9a612b5d8a116c67b89d9c34
fareloz <fareloz@gmail.com>
2016-12-24 00:06:35 +0200

    Add process index to state as primary key. Use process name as property

--- State.cs
+++ State.cs
@@ -4,17 +4,33 @@ namespace AttachToolbar
 {
     internal static class State
     {
-        public static string ProcessName = "";
+        public static int ProcessIndex = -1;
         public static List<string> ProcessList = new List<string>();
         public static EngineType EngineType = EngineType.Native;
         public static bool IsAttached = false;
+
+        public static string ProcessName
+        {
+            get
+            {
+                string processName = ProcessIndex >= 0
+                        ? ProcessList[ProcessIndex]
+                        : "";
+                return processName;
+            }
+
+            set
+            {
+                ProcessIndex = ProcessList.IndexOf(value);
+            }
+        }
 
         public static void Clear()
         {
             ProcessList.Clear();
             EngineType = EngineType.Native;
             IsAttached = false;
-            ProcessName = "";
+            ProcessIndex = -1;
         }
     }
 }
