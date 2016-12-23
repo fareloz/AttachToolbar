1d9271cd630e06b45da89b5fd93657fe20e13be2
fareloz <fareloz@gmail.com>
2016-12-24 00:55:59 +0200

    Restore designer code

--- OptionsGeneralWindow.cs
+++ OptionsGeneralWindow.cs
@@ -7,101 +7,104 @@ namespace AttachToolbar
         public void Initialize()
         {
             InitializeComponent();
+            foreach (var processName in State.ProcessList)
+                listviewProcessList.Items.Add(processName);
         }
 
         private void InitializeComponent()
         {
-            buttonAdd = new Button();
-            textboxProcessName = new TextBox();
-            listviewProcessList = new ListView();
-            labelProcessName = new Label();
-            buttonRemove = new Button();
-            buttonClear = new Button();
-            columnProcessName = new ColumnHeader();
-            SuspendLayout();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.textboxProcessName = new System.Windows.Forms.TextBox();
+            this.listviewProcessList = new System.Windows.Forms.ListView();
+            this.columnProcessName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.labelProcessName = new System.Windows.Forms.Label();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
             // 
             // buttonAdd
             // 
-            buttonAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            buttonAdd.Location = new System.Drawing.Point(361, 10);
-            buttonAdd.Name = "buttonAdd";
-            buttonAdd.Size = new System.Drawing.Size(108, 31);
-            buttonAdd.TabIndex = 1;
-            buttonAdd.Text = "Add";
-            buttonAdd.UseVisualStyleBackColor = true;
-            buttonAdd.Click += buttonAdd_Click;
+            this.buttonAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonAdd.Location = new System.Drawing.Point(361, 10);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(108, 31);
+            this.buttonAdd.TabIndex = 1;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
             // 
             // textboxProcessName
             // 
-            textboxProcessName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            textboxProcessName.Location = new System.Drawing.Point(146, 12);
-            textboxProcessName.Name = "textboxProcessName";
-            textboxProcessName.Size = new System.Drawing.Size(203, 26);
-            textboxProcessName.TabIndex = 0;
+            this.textboxProcessName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textboxProcessName.Location = new System.Drawing.Point(146, 12);
+            this.textboxProcessName.Name = "textboxProcessName";
+            this.textboxProcessName.Size = new System.Drawing.Size(203, 26);
+            this.textboxProcessName.TabIndex = 0;
             // 
             // listviewProcessList
             // 
-            listviewProcessList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            columnProcessName});
-            listviewProcessList.GridLines = true;
-            listviewProcessList.HeaderStyle = ColumnHeaderStyle.Nonclickable;
-            listviewProcessList.Location = new System.Drawing.Point(20, 51);
-            listviewProcessList.Name = "listviewProcessList";
-            listviewProcessList.Size = new System.Drawing.Size(329, 202);
-            listviewProcessList.TabIndex = 4;
-            listviewProcessList.UseCompatibleStateImageBehavior = false;
-            listviewProcessList.View = View.Details;
-            foreach (var processName in State.ProcessList)
-                listviewProcessList.Items.Add(processName);
-            // 
-            // labelProcessName
-            // 
-            labelProcessName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            labelProcessName.Location = new System.Drawing.Point(17, 15);
-            labelProcessName.Name = "labelProcessName";
-            labelProcessName.Size = new System.Drawing.Size(128, 28);
-            labelProcessName.TabIndex = 5;
-            labelProcessName.Text = "Process name:";
-            // 
-            // buttonRemove
-            // 
-            buttonRemove.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            buttonRemove.Location = new System.Drawing.Point(361, 51);
-            buttonRemove.Name = "buttonRemove";
-            buttonRemove.Size = new System.Drawing.Size(108, 31);
-            buttonRemove.TabIndex = 2;
-            buttonRemove.Text = "Remove";
-            buttonRemove.UseVisualStyleBackColor = true;
-            buttonRemove.Click += buttonRemove_Click;
-            // 
-            // buttonClear
-            // 
-            buttonClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            buttonClear.Location = new System.Drawing.Point(361, 88);
-            buttonClear.Name = "buttonClear";
-            buttonClear.Size = new System.Drawing.Size(108, 31);
-            buttonClear.TabIndex = 3;
-            buttonClear.Text = "Clear";
-            buttonClear.UseVisualStyleBackColor = true;
-            buttonClear.Click += buttonClear_Click;
+            this.listviewProcessList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnProcessName});
+            this.listviewProcessList.FullRowSelect = true;
+            this.listviewProcessList.GridLines = true;
+            this.listviewProcessList.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listviewProcessList.HideSelection = false;
+            this.listviewProcessList.Location = new System.Drawing.Point(20, 51);
+            this.listviewProcessList.Name = "listviewProcessList";
+            this.listviewProcessList.Size = new System.Drawing.Size(329, 202);
+            this.listviewProcessList.TabIndex = 4;
+            this.listviewProcessList.UseCompatibleStateImageBehavior = false;
+            this.listviewProcessList.View = System.Windows.Forms.View.Details;
             // 
             // columnProcessName
             // 
-            columnProcessName.Text = "Process name";
-            columnProcessName.Width = 325;
+            this.columnProcessName.Text = "Process name";
+            this.columnProcessName.Width = 325;
+            // 
+            // labelProcessName
+            // 
+            this.labelProcessName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelProcessName.Location = new System.Drawing.Point(17, 15);
+            this.labelProcessName.Name = "labelProcessName";
+            this.labelProcessName.Size = new System.Drawing.Size(128, 28);
+            this.labelProcessName.TabIndex = 5;
+            this.labelProcessName.Text = "Process name:";
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonRemove.Location = new System.Drawing.Point(361, 51);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(108, 31);
+            this.buttonRemove.TabIndex = 2;
+            this.buttonRemove.Text = "Remove";
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonClear.Location = new System.Drawing.Point(361, 88);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(108, 31);
+            this.buttonClear.TabIndex = 3;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
             // 
             // OptionsGeneralWindow
             // 
-            Controls.Add(buttonClear);
-            Controls.Add(buttonRemove);
-            Controls.Add(labelProcessName);
-            Controls.Add(listviewProcessList);
-            Controls.Add(textboxProcessName);
-            Controls.Add(buttonAdd);
-            Name = "OptionsGeneralWindow";
-            Size = new System.Drawing.Size(499, 277);
-            ResumeLayout(false);
-            PerformLayout();
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonRemove);
+            this.Controls.Add(this.labelProcessName);
+            this.Controls.Add(this.listviewProcessList);
+            this.Controls.Add(this.textboxProcessName);
+            this.Controls.Add(this.buttonAdd);
+            this.Name = "OptionsGeneralWindow";
+            this.Size = new System.Drawing.Size(499, 277);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         private void buttonAdd_Click(object sender, System.EventArgs e)
